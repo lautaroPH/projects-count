65a921ab882bbec29d8ad29399991ff5cfa534df
lautaroPH <lautaroph@gmail.com>
2022-03-28 15:13:46 -0300

    remove trim

--- Validations/LinkFormValidation.js
+++ Validations/LinkFormValidation.js
@@ -4,20 +4,14 @@ export const linkFormSchema = yup.object().shape({
   title: yup
     .string()
     .strict(true)
-    .trim('No debe tener espacios por detras ni adelante')
     .min(5, 'Minimo 5 caracteres')
-    .max(40, 'Maximo 40 caracteres')
     .required('Campo Requerido'),
   link: yup.string().url('Url no valida').required('Campo Requerido'),
   description: yup
     .string()
     .strict(true)
-    .trim('No debe tener espacios por detras ni adelante')
     .required('Campo Requerido')
     .min(5, 'Minimo 5 caracteres'),
   githubRepo: yup.string().url('Url no valida'),
-  tecnologies: yup
-    .string()
-    .strict(true)
-    .trim('No debe tener espacios por detras ni adelante'),
+  tecnologies: yup.string().strict(true),
 });
