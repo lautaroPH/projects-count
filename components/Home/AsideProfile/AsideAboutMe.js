c36e048d9778fb2acf51fe91aa41b3154277a693
lautaroPH <lautaroph@gmail.com>
2022-03-28 21:21:59 -0300

    start asideAboutMe

--- components/Home/AsideProfile/AsideAboutMe.js
+++ components/Home/AsideProfile/AsideAboutMe.js
@@ -1,32 +1,37 @@
 import useUser from 'hooks/useUser';
-import Image from 'next/image';
 import AsideActivity from './AsideActivity';
+
 const AsideAboutMe = () => {
   const user = useUser();
   return (
     <div>
-      <div className="flex justify-end w-full">
-        <div className="">
-          <div className="flex bg-white dark:bg-gray-900 w-full">
-            <div className="flex flex-col items-center p-2">
-              <h2 className="text-violet-700 dark:text-white text-xl">
+      <div className="flex justify-end w-full ">
+        <div className="w-[75%]">
+          <div className="bg-white dark:bg-gray-900 w-full px-2">
+            <div className="flex border-b-2 flex-col items-center p-2">
+              <h2 className="text-violet-700 dark:text-white text-xl text-center">
                 Mi perfil
               </h2>
-              <div className="flex">
-                <div className="h-16 w-16">
-                  {user?.avatar && (
-                    <Image
-                      src={user?.avatar}
-                      alt={user?.username}
-                      width={150}
-                      height={150}
-                      className="rounded-full"
-                    />
-                  )}
-                </div>
-                <h4 className="">{user?.username}</h4>
+              <div className="mt-2 text-center">
+                <h4 className="font-semibold text-lg">{user?.username}</h4>
               </div>
-              <p>Sobre mi</p>
+              <div className="flex cursor-pointer">
+                <p className="mr-2 text-gray-500">Profesion</p>
+                <p className="text-violet-600 hover:underline">Editar</p>
+              </div>
+            </div>
+            <div className="px-4 py-2">
+              <h2 className=" font-semibold dark:text-white  text-center">
+                Sobre mi
+              </h2>
+              <p className="">
+                orem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essent
+              </p>
             </div>
           </div>
         </div>
