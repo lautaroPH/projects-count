269a4c4e46f72f903fd9c291f150f5f3b156acca
lautaroPH <lautaroph@gmail.com>
2022-04-03 15:05:59 -0300

    add pagination and buttons

--- components/Home/Main/Comments/Comment.js
+++ components/Home/Main/Comments/Comment.js
@@ -3,6 +3,7 @@ import { useTimeAgo } from 'hooks/useTimeAgo';
 import useUser from 'hooks/useUser';
 import Image from 'next/image';
 import { useState } from 'react';
+import AnswersList from './AnswersList';
 import ButtonDeleteComment from './ButtonDeleteComment';
 
 const Comment = ({
@@ -13,8 +14,12 @@ const Comment = ({
   timestamp,
   userId,
   username,
+  index,
+  seeAllComments,
 }) => {
   const user = useUser();
+  const [openAllComment, setOpenAllComment] = useState(false);
+
   if (timestamp !== null) {
     const createdAt = new Date(parseInt(timestamp?.seconds * 1000));
   }
@@ -23,52 +28,54 @@ const Comment = ({
     createdAt !== undefined && createdAt
   );
 
-  const [openAllComment, setOpenAllComment] = useState(false);
-
   return (
-    <div className="flex justify-center px-4 py-2">
-      <div className="h-12 w-12 mr-2 mt-2">
-        <Image
-          className="rounded-full"
-          src={avatar}
-          height={150}
-          width={150}
-          alt={comment}
-        />
-      </div>
-      <div className="bg-gray-100 w-[90%] rounded-lg p-2 relative">
-        <div className="flex justify-between">
-          <div>
-            <p className="font-semibold">{username}</p>
-            <p className="text-xs sm:text-sm font-light mb-1">
-              <time title={createdAtFormated}>{timeago}</time>
-            </p>
-          </div>
-          {userId === user?.id && (
-            <p className="-mt-1">
-              <ButtonDeleteComment
-                commentId={commentId}
-                linkId={linkId}
-                userId={user?.id}
-              />
-            </p>
-          )}
+    <>
+      <div className="flex justify-center px-4 pb-1">
+        <div className="w-12 h-12 mt-2 mr-2">
+          <Image
+            className="rounded-full"
+            src={avatar}
+            height={150}
+            width={150}
+            alt={comment}
+          />
         </div>
+        <div className="bg-gray-100 dark:bg-gray-800 w-[90%] rounded-lg p-2 relative">
+          <div className="flex justify-between">
+            <div>
+              <p className="font-semibold">{username}</p>
+              <p className="mb-1 text-xs font-light dark:text-gray-300 sm:text-sm">
+                <time title={createdAtFormated}>{timeago}</time>
+              </p>
+            </div>
+            {userId === user?.id && (
+              <p className="-mt-1">
+                <ButtonDeleteComment
+                  commentId={commentId}
+                  linkId={linkId}
+                  userId={user?.id}
+                />
+              </p>
+            )}
+          </div>
 
-        <p className="overflow-hidden text-sm">
-          {comment.substring(0, 260)}
-          {comment.length > 240 && !openAllComment && (
-            <button
-              onClick={() => setOpenAllComment(true)}
-              className=" pl-1 text-sm text-violet-600 hover:text-violet-700 hover:underline"
-            >
-              ...ver más
-            </button>
-          )}
-          {openAllComment && <>{comment.substring(260, 1250)}</>}
-        </p>
+          <p className="overflow-hidden text-sm">
+            {comment.substring(0, 260)}
+            {comment.length > 240 && !openAllComment && (
+              <button
+                onClick={() => setOpenAllComment(true)}
+                className="pl-1 text-sm transition-all duration-150 ease-in text-violet-600 dark:text-gray-300 hover:text-violet-700 dark:hover:text-gray-400 hover:underline"
+              >
+                ...ver más
+              </button>
+            )}
+            {openAllComment && <>{comment.substring(260, 1250)}</>}
+          </p>
+        </div>
       </div>
-    </div>
+
+      <AnswersList linkId={linkId} commentId={commentId} index={index} />
+    </>
   );
 };
 
--- components/Home/Main/Footer.js
+++ components/Home/Main/Footer.js
@@ -2,17 +2,21 @@ import React from 'react';
 import AllButtons from './Buttons/AllButtons';
 import LikesCommentsNumber from './LikesCommentsNumber';
 import { useState, useEffect } from 'react';
-import CommentsList from './Comments/CommentsList';
 import { getLikes } from 'firebaseFunction/getLikes';
-import { getComments } from 'firebaseFunction/getComments';
+import { getAllComments } from 'firebaseFunction/getAllComments';
+import FormComment from './Forms/FormComment';
+import Comment from './Comments/Comment';
+import useUser from 'hooks/useUser';
 
 const Footer = ({ id, username, dataUserLike, githubRepo, link, title }) => {
   const [likes, setLikes] = useState([]);
   const [comments, setComments] = useState([]);
   const [openCommentInput, setOpenCommentInput] = useState(false);
+  const [seeAllComments, setSeeAllComments] = useState(false);
+  const user = useUser();
 
   useEffect(() => getLikes(id, setLikes), [id]);
-  useEffect(() => getComments(id, setComments), [id]);
+  useEffect(() => getAllComments(id, setComments), [id]);
 
   return (
     <>
@@ -35,13 +39,54 @@ const Footer = ({ id, username, dataUserLike, githubRepo, link, title }) => {
       />
 
       {openCommentInput && (
-        <CommentsList
-          id={id}
-          title={title}
-          openCommentInput={openCommentInput}
-          comments={comments}
-          setComments={setComments}
-        />
+        <div className="mb-3">
+          {user && <FormComment id={id} title={title} />}
+
+          {comments.map((comment, i) => {
+            return i <= 9 ? (
+              <Comment
+                key={comment.id}
+                linkId={id}
+                index={i}
+                commentId={comment.id}
+                avatar={comment.data().avatar}
+                comment={comment.data().comment}
+                timestamp={comment.data().timestamp}
+                userId={comment.data().userId}
+                username={comment.data().username}
+                seeAllComments={seeAllComments}
+              />
+            ) : (
+              seeAllComments && (
+                <Comment
+                  key={comment.id}
+                  linkId={id}
+                  index={i}
+                  commentId={comment.id}
+                  avatar={comment.data().avatar}
+                  comment={comment.data().comment}
+                  timestamp={comment.data().timestamp}
+                  userId={comment.data().userId}
+                  username={comment.data().username}
+                  seeAllComments={seeAllComments}
+                />
+              )
+            );
+          })}
+          {comments.length <= 0 && (
+            <p className="px-2 py-1 ml-2 text-base font-semibold text-gray-500 dark:text-gray-300">
+              No hay comentarios para ver
+            </p>
+          )}
+          {!seeAllComments && comments.length > 10 && (
+            <button
+              className="px-2 py-1 ml-2 text-sm font-semibold text-gray-500 transition-all duration-200 ease-in-out rounded-md hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
+              onClick={() => setSeeAllComments(true)}
+            >
+              Ver todos los comentários
+            </button>
+          )}
+        </div>
       )}
     </>
   );
