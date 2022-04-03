a1f1e5ab2bb0d3d01826be2089456bf08163073c
lautaroPH <lautaroph@gmail.com>
2022-04-03 14:44:00 -0300

    add verification if the linkPost exist

--- firebaseFunction/uploadComment.js
+++ firebaseFunction/uploadComment.js
@@ -1,17 +1,40 @@
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  serverTimestamp,
+} from 'firebase/firestore';
 import { db } from 'firebaseMain/firebase';
+import Swal from 'sweetalert2';
+import { deleteLike } from './deleteLike';
 import { uploadUserComment } from './uploadUserComment';
 
-export const uploadComment = async (id, user, comment, title) => {
+export const uploadComment = async (id, user, comment, title, router) => {
   const { id: userId, username, avatar } = user;
   const commentTrim = comment.trim();
-  const commentRef = await addDoc(collection(db, 'links', id, 'comments'), {
-    userId,
-    username,
-    avatar,
-    comment: commentTrim,
-    timestamp: serverTimestamp(),
-  });
 
-  uploadUserComment(id, userId, comment, title, commentRef.id);
+  const querySnapshot = doc(db, 'links', id);
+
+  const linkRef = await getDoc(querySnapshot);
+
+  if (linkRef.data()) {
+    const commentRef = await addDoc(collection(db, 'links', id, 'comments'), {
+      userId,
+      username,
+      avatar,
+      comment: commentTrim,
+      timestamp: serverTimestamp(),
+    });
+    await uploadUserComment(id, userId, comment, title, commentRef.id);
+  } else {
+    Swal.fire({
+      text: 'Este link no existe más',
+      icon: 'error',
+      timer: '3000',
+    });
+    router.reload();
+
+    deleteLike(id, userId);
+  }
 };
