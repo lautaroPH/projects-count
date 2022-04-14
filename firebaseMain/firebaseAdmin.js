83782a4937b9ae2003dedeac8ba1638f77e9ff82
lautaroPH <lautaroph@gmail.com>
2022-04-14 13:40:49 -0300

    add firebaseAdmin

---
+++ firebaseMain/firebaseAdmin.js
@@ -0,0 +1,10 @@
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+
+const { getFirestore } = require('firebase-admin/firestore');
+try {
+  initializeApp({
+    credential: applicationDefault(),
+  });
+} catch (e) {}
+const db = getFirestore();
+export { db };
