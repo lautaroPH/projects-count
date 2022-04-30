4ebce89c20f252d06f3dc648ecaae3514e4dd398
lautaroPH <lautaroph@gmail.com>
2022-04-30 20:23:15 -0300

    add encripted keys for firebase admin

--- firebaseMain/firebaseAdmin.js
+++ firebaseMain/firebaseAdmin.js
@@ -1,11 +1,19 @@
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
-
+var admin = require('firebase-admin');
+var ncrypt = require('ncrypt-js');
+const { initializeApp } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+
+const data = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+var _secretKey = process.env.SECRET_KEY;
+
+var ncryptObject = new ncrypt(_secretKey);
+var decryptedData = ncryptObject.decrypt(data);
 
 try {
   initializeApp({
-    credential: applicationDefault(),
+    credential: admin.credential.cert(decryptedData),
   });
 } catch (e) {}
+
 const db = getFirestore();
 export { db };
